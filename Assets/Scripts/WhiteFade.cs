cdbb5b72737ad322bae00c7aaa7afcf823db73f0
Bruno Araujo <araujo.contato@gmail.com>
2016-09-04 13:49:58 -0300

    update for itch.io

---
+++ Assets/Scripts/WhiteFade.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhiteFade : MonoBehaviour {
+
+    static public bool fadeIsOver = false;
+
+    public void setFade() {
+        fadeIsOver = true;
+        gameObject.SetActive(false);
+    }
+}
